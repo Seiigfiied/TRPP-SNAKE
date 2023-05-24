f93071828415dd2e812d4f95691092fa31a95602
DanselSoul <112869364+DanselSoul@users.noreply.github.com>
2023-05-24 19:12:42 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='snake_game',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='',
+    install_requires=open('requirements.txt').readlines()
+)
